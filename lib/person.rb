33fba3dbb50249fd2c932ba08e8f918c8e754156
Urfan Guliyev <urfan196@yahoo.com>
2019-09-22 19:07:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -51,9 +51,11 @@ end
 def start_conversation (person, topic)
   if topic == 'politics'
     person.happiness -= 2
+    self.happiness -= 2
     "blah blah partisan blah lobbyist"
   elsif topic == 'weather'
     person.happiness += 1
+    self.happiness += 1
     "blah blah sun blah rain"
   else
     "blah blah blah blah blah"
