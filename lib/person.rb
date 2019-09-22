d23c65f175a87d65be49aaede21a397211ebe200
Urfan Guliyev <urfan196@yahoo.com>
2019-09-22 18:22:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
-# your code goes here
+class Person
+
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+
+def initialize(name)
+  @name = name
+  @bank_account = 25
+  @happiness = 8
+  @hygiene = 8
+end
+
+def happiness=(hygiene)
+@happiness = happiness.clamp(0,10)
+end
+
+def hygiene=(hygiene)
+@hygiene = hygiene.clamp(0,10)
+end
+
+end
